c6a2a0957790c6cb2c771ed892e1f6a9a0ecd1ab
bearcoretv <77353437+bearcoretv@users.noreply.github.com>
2021-03-15 13:19:31 -0600

    Add files via upload

---
+++ Homework 9/pallet.js
@@ -0,0 +1,34 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(6, 105, 33);
+    fill(133, 105, 64);
+    ellipse(250, 250, 200, 250);
+    triangle(235, 250, 250, 235, 265, 250);
+    fill(255, 255, 255);
+    circle(200, 175, 50);
+    fill(255, 255, 255);
+    circle(300, 175, 50);
+    fill(61, 38, 4);
+    circle(200, 175, 25);
+    fill(61, 38, 4);
+    circle(300, 175, 25);
+    fill(0, 0, 0);
+    line(200, 145, 225, 150);
+    fill(0, 0, 0);
+    line(275, 150, 300, 145);
+    fill(242, 145, 189);
+    rect(200, 300, 100, 50);
+    fill(0, 0, 0);
+    rect(225, 70, 50, 60);
+    point(200,175)
+    point(300,175)
+    textSize(32);
+    text('Simulacrum', 170, 420);
+    textSize(16);
+    text('By Teddy J.', 220, 440);
+}
